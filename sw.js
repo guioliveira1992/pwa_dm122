a54c875c20324226343ca80ca83a26bbd5b94636
Guilherme Oliveira <guilhermeoliveira@MacBook-Pro-de-Guilherme.local>
2022-10-29 12:26:23 -0300

    Adjusts some points

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "app-shell-v2";
+const staticCache = "app-shell-v2";
 
 const assetsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
@@ -6,70 +6,63 @@ const assetsToCache = [
   'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
   'https://fonts.googleapis.com/css?family=Roboto:400,700',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'assets/images/pwa-logo.png',
-  'assets/js/material.min.js',
-  'assets/style.css',
-  'app.js',
-  'actions.js',
-  'handler.js',
-  'store.js',
-  'favicon.ico',
-  'index.html',
-  '/'
+  './assets/images/pwa-logo.png',
+  './assets/js/material.min.js',
+  './assets/style.css',
+  './app.js',
+  './actions.js',
+  './handler.js',
+  './store.js',
+  './favicon.ico',
+  './index.html',
+  './'
 ];
 
-function removeOldCache(key) {
-  if (key !== cacheName) {
-    console.log(`[Service Worker] Removing old cache: ${key}`);
-    return caches.delete(key);
-  }
-}
-
-async function cacheCleanup() {
-  const keyList = await caches.keys();
-  return Promise.all(keyList.map(removeOldCache));
-}
-
 async function cacheStaticAssets() {
-  const cache = await caches.open(cacheName);
-  assetsToCache.forEach((assert)=>{
+  const cache = await caches.open(staticCache);
+  assetsToCache.forEach((assert) =>{
     return cache.add(assert)
   })
   // return cache.addAll(assetsToCache);
 }
 
-self.addEventListener('install', (event) => {
-  console.log('[Service Worker] Installing Service Worker...', event);
-  event.waitUntil(cacheStaticAssets());
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[Service Worker] Activating Service Worker...', event);
-  event.waitUntil(cacheCleanup());
-  return self.clients.claim();
-});
-
 async function networkFirst(request) {
   try {
+    console.log("executing network")
     return await fetch(request);
   } catch (error) {
-    const cache = await caches.open(cacheName);
+    console.log("[Service Worker] network error", error);
+    const cache = await caches.open(staticCache);
     return cache.match(request);
   }
 }
 
+self.addEventListener("install", (event) => {
+  console.log("[Service Worker] Installing service worker");
+  event.waitUntil(cacheStaticAssets());
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  console.log("[Service Worker] Activating service worker!");
+  return self.clients.claim();
+});
+
 async function cacheFirst(request) {
   try {
-    const cache = await caches.open(cacheName);
+    console.log("executing cache")
+    const cache = await caches.open(staticCache);
+    console.log(cache)
+    console.log(request)
     const response = await cache.match(request);
+    console.log(response)
     return response || fetch(request);
   } catch (error) {
     console.log(error);
   }
 }
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
+  console.log("[Service Worker] Fetch event worker!", event.request.url);
   event.respondWith(cacheFirst(event.request));
 });
-
